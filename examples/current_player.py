cbcd87e8e2db60eebcd886d4813830621bc8758e
Julheer <admin@julheer.ru>
2021-03-26 23:24:51 +0300

    Added docstring, update todos.

--- examples/current_player.py
+++ examples/current_player.py
@@ -2,4 +2,4 @@ from spotiapi import SpotifyAPI
 spotify = SpotifyAPI('spotify_token')
 
 request_track = spotify.getUserCurrentTrack()
-print(f'Listening now {request_track[1]} 🎧')
+print(f'Listening now {request_track[1]}')
