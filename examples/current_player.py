3bab34275c54d49932e51778fc5c4ca43733aa44
Aleksey Demchenkov <admin@julheer.ru>
2021-03-27 11:43:48 +0300

    Version 1.2.2: bug fixes

--- examples/current_player.py
+++
@@ -1,5 +0,0 @@
-from spotiapi import SpotifyAPI
-spotify = SpotifyAPI('spotify_token')
-
-request_track = spotify.getUserCurrentTrack()
-print(f'Listening now {request_track[1]}')
--- examples/get_user.py
+++
@@ -1,5 +0,0 @@
-from spotiapi import SpotifyAPI
-spotify = SpotifyAPI('spotify_token')
-
-request_user = spotify.getUserData()
-print(f'Hello, {request_user["display_name"]}! You have {request_user["followers"]["total"]} subs.')
